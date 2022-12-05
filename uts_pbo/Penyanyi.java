abfaac8cb8f33642664825df89c69deac686f9d2
HardiantiRamli <119602779+HardiantiRamli@users.noreply.github.com>
2022-12-05 12:34:49 +0700

    uploud uts

---
+++ uts_pbo/Penyanyi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts_pbo;
+
+/**
+ *
+ * @author hardi
+ */
+public class Penyanyi {
+    public static void main(String[]args){
+       Pop pop = new Pop("Black Pink", "How You Like That");
+       Dangdut dangdut = new dangdut();
+       
+       pop.singer("Eclat Story");
+       pop.song("Bentuk Cinta");
+       pop.cetakLabel();
+       
+       String penyanyi = "Joko Chandra";
+       penyanyi = dangdut.penyanyi;
+       dangdut.song("Via Vallen", "Kopi Dangdut");
+       dangdut.cetakLabel();
+}
---
+++ uts_pbo/Pop.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts_pbo;
+
+/**
+ *
+ * @author hardi
+ */
+public class Pop {
+   
+    String Penyanyi = "Raisa";
+    String lagu ="Kali Kedua";
+    
+    Pop(){
+        
+    }
+    Pop (String penyanyi, String lagu){
+        this.Penyanyi = penyanyi;
+        this.lagu = lagu;
+        System.out.println(this.Penyanyi+"<=>"+this.lagu);
+    }
+    
+    public void singer(String penyanyi){
+        this.Penyanyi = penyanyi;
+    }
+    
+    protected void song(String lagu){
+        this.lagu = lagu;
+    }
+    
+    private Void cetakLabel(){
+       System.out.println(this.Penyanyi+" <=> "+this.lagu);
+    }
+}
+
